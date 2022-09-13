e950c2a1d47311a416d11dd57b70f4e89b7c0420
Jonathan Carpena <63978806+jonathancarpena@users.noreply.github.com>
2022-09-12 17:24:02 -0700

    Fixed loading time

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,12 +1,13 @@
-
 // Uncomment for Develpment
 // const BASE_API = 'http://localhost:5000/api'
 
-const BASE_API = 'https://pokemon-guesser-web.herokuapp.com/api'
+const BASE_API = 'https://pokemon-guesser-web.herokuapp.com/api';
 
 const urls = {
-    scores: `${BASE_API}/scores`,
-    addScore: `${BASE_API}/scores/add`,
-}
+  audio: `/cries`,
+  image: `/shape`,
+  scores: `${BASE_API}/scores`,
+  addScore: `${BASE_API}/scores/add`,
+};
 
-export default urls
+export default urls;
