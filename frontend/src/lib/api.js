0d072886e5d4d720f5882b5bb2323b29a4218ea2
Jonathan <jonathancarpena99@gmail.com>
2022-11-28 08:56:22 -0800

    Updated Server Host

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -2,7 +2,7 @@
 // Uncomment for Develpment
 // const BASE_API = 'http://localhost:5000/api'
 
-const BASE_API = 'https://pokemon-guesser-web.herokuapp.com/api'
+const BASE_API = 'https://pokemon-guesser-api.onrender.com/api'
 
 const urls = {
     scores: `${BASE_API}/scores`,
